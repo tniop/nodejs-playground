cda06a75edacf26a6b640e63342ff413458f789c
DongHo Lee <flitto@Flittoui-MacBookPro.local>
2025-08-08 12:05:58 +0900

    refactor: improve error handling

--- prac/scripts/parallel-promise.js
+++ prac/scripts/parallel-promise.js
@@ -1,11 +1,11 @@
 import ApiError from '../libs/api-error.js'
 
 function processJob (seconds) {
-  return new Promise((resolve, reject) => {
-    if (!Number.isInteger(seconds) || seconds <= 0) {
-      return reject(ApiError.BAD_REQUEST(new Error(`'${seconds}' is not a natural number.`)))
-    }
+  if (!Number.isInteger(seconds) || seconds <= 0) {
+    return Promise.reject(ApiError.BAD_REQUEST(new Error(`'${seconds}' is not a natural number.`)))
+  }
 
+  return new Promise((resolve, reject) => {
     console.time(`timer-${seconds}`)
     setTimeout(() => {
       console.timeEnd(`timer-${seconds}`)
